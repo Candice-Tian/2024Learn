2ed3007998624a1d713159c870c3d054694b1453
Candice-Tian <tian779713952@163.com>
2024-05-20 22:20:05 +0800

    Use DB create models

---
+++ NetCoreLearn/Models/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCoreLearn.Models;
+
+public partial class Coupon
+{
+    public int Id { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public DateOnly? Expiration { get; set; }
+}
